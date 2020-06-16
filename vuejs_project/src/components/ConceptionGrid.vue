d32c5337df90e4b91f97afd4efa000f1db31fcb9
Arthur Peyrichou <arthurpeyrichou@yahoo.fr>
2020-06-16 16:57:03 +0200

    Task 12 DragAndDrop of FDComp inside the conception grid, next step: rendering

--- vuejs_project/src/components/ConceptionGrid.vue
+++ vuejs_project/src/components/ConceptionGrid.vue
@@ -4,7 +4,7 @@
     <h3>Conception Grid</h3>
     </div>
     <div class="board">
-      <svg class="grid" @dragover.prevent v-on:drop="drop($event)">
+      <svg id="conception-grid-svg" class="grid" viewBox="0,0,1000,1000" @dragover.prevent v-on:drop="drop($event)">
       </svg>
     </div>
   </div>
@@ -13,9 +13,18 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import { FDComponent } from '../models/FDComponent';
+import * as d3 from "d3";
 
 @Component
 export default class ConceptionGrid extends Vue {
+  // eslint-disable-next-line to ignore the next line.
+  private fdCompToDrop: any = undefined;
+
+  constructor() {
+    super();
+    //Tthis way we execute the code after the redering of the template
+    this.$nextTick( () => {this.initSvg();});
+  }
 
   /**
    * Call when a flowdata component is drop inside the <svg>
@@ -23,9 +32,67 @@ export default class ConceptionGrid extends Vue {
   public drop(event: DragEvent) {
       console.log("drop")
       if(event.dataTransfer != null && event.dataTransfer != undefined){
-        const theFDComp = FDComponent.fromString(event.dataTransfer.getData("text"))
-        console.log(theFDComp)
+        this.fdCompToDrop = FDComponent.fromString(event.dataTransfer.getData("text"));
       }
+  }
+
+  private initSvg() {
+    const addComp = (x: number, y: number) =>  {
+      if(this.fdCompToDrop !== undefined) {
+        d3.select("#conception-grid-svg")
+          .append("g")
+          .attr("stroke", "black")
+          .attr("stroke-width", 1.5)
+          .attr("style", "cursor:pointer;")
+          .append("circle")
+          .attr("r", 5)
+          .attr("fill", this.fdCompToDrop.getColor())
+          .attr("cx", () => {
+            if(x < 10){
+              return 10;
+            } else if(x > 990){
+              return 990;
+            }
+            return x
+          })
+          .attr("cy", () => { 
+            if(y < 10){
+              return 10;
+            } else if(y > 990){
+              return 990;
+            }
+            return y
+          });
+          
+        const dragHandler = d3.drag()
+            .on("drag", function () {
+                d3.select(this)
+                    .attr("cx", () => {
+                      if(d3.event.x < 10){
+                        return 10;
+                      } else if(d3.event.x > 990){
+                        return 990;
+                      }
+                      return d3.event.x})
+                    .attr("cy", () => { 
+                      if(d3.event.y < 10){
+                        return 10;
+                      } else if(d3.event.y > 990){
+                        return 990;
+                      }
+                      return d3.event.y})
+            });
+
+        dragHandler(d3.select("#conception-grid-svg").selectAll("circle"));
+        this.fdCompToDrop = undefined;
+      }
+    };
+    
+    d3.select("#conception-grid-svg")
+      .on("mousemove", function () {
+        const target = d3.mouse(d3.event.currentTarget);
+        addComp(target[0], target[1]);
+      });
   }
 }
 </script>
