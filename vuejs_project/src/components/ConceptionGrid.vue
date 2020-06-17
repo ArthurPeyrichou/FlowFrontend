b1d8f83992f5c3d47d2ac7810f7689abb9103cc7
Arthur Peyrichou <arthurpeyrichou@yahoo.fr>
2020-06-17 10:57:17 +0200

    Task 11-12 Optimisation of components rendering

--- vuejs_project/src/components/ConceptionGrid.vue
+++ vuejs_project/src/components/ConceptionGrid.vue
@@ -4,7 +4,7 @@
     <h3>Conception Grid</h3>
     </div>
     <div class="board">
-      <svg id="conception-grid-svg" class="grid" viewBox="0,0,1000,1000" @dragover.prevent v-on:drop="drop($event)">
+      <svg id="conception-grid-svg" class="grid" viewBox="0,0,5000,5000" @dragover.prevent v-on:drop="drop($event)">
       </svg>
     </div>
   </div>
@@ -19,6 +19,7 @@ import * as d3 from "d3";
 export default class ConceptionGrid extends Vue {
   // eslint-disable-next-line to ignore the next line.
   private fdCompToDrop: any = undefined;
+  private componentList: Array<string> = [];
 
   constructor() {
     super();
@@ -36,54 +37,277 @@ export default class ConceptionGrid extends Vue {
       }
   }
 
+  private makeId(length: number): string {
+    let result= '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+  }
+
   private initSvg() {
     const addComp = (x: number, y: number) =>  {
       if(this.fdCompToDrop !== undefined) {
-        d3.select("#conception-grid-svg")
+        const compHeight = 60;
+        const compWidth = 115 + this.fdCompToDrop.getTitle().length * 10;
+        const svgGridBorder = 10;
+        const svgMax = 5000;
+        
+        let newId = this.makeId(10);
+        while(this.componentList.includes(newId)){
+          newId = this.makeId(10);
+        }
+
+        const g = d3.select("#conception-grid-svg")
           .append("g")
+          .attr("id", "comp-" + newId)
           .attr("stroke", "black")
           .attr("stroke-width", 1.5)
-          .attr("style", "cursor:pointer;")
-          .append("circle")
-          .attr("r", 5)
+          .attr("style", "cursor:pointer;");
+        
+        g.append("rect")
+          .attr("id", "rect-" + newId)
           .attr("fill", this.fdCompToDrop.getColor())
-          .attr("cx", () => {
-            if(x < 10){
+          .attr("height", compHeight)
+          .attr("width", compWidth)
+          .attr("rx",5)
+          .attr("x", () => {
+            if(x < (svgGridBorder + (compWidth / 2))){
               return 10;
-            } else if(x > 990){
-              return 990;
+            } else if(x > (svgMax - svgGridBorder - (compWidth / 2))){
+              return svgMax - svgGridBorder - compWidth;
             }
-            return x
+            return x - (compWidth / 2);
           })
-          .attr("cy", () => { 
-            if(y < 10){
-              return 10;
-            } else if(y > 990){
-              return 990;
+          .attr("y", () => { 
+            if(y < (svgGridBorder + (compHeight / 2))){
+              return svgGridBorder;
+            } else if(y > (svgMax - svgGridBorder - (compHeight / 2))){
+              return svgMax - svgGridBorder - compHeight;
             }
-            return y
+            return y - (compHeight / 2);
           });
-          
-        const dragHandler = d3.drag()
-            .on("drag", function () {
-                d3.select(this)
-                    .attr("cx", () => {
-                      if(d3.event.x < 10){
-                        return 10;
-                      } else if(d3.event.x > 990){
-                        return 990;
-                      }
-                      return d3.event.x})
-                    .attr("cy", () => { 
-                      if(d3.event.y < 10){
-                        return 10;
-                      } else if(d3.event.y > 990){
-                        return 990;
-                      }
-                      return d3.event.y})
+
+        g.append("text")
+          .attr("id", "title-text-" + newId)
+          .attr("fill", "black")
+          .html(this.fdCompToDrop.getTitle())
+          .attr("x", () => {
+            if(x < (svgGridBorder + (compWidth / 2))){
+              return svgGridBorder + 95;
+            } else if(x > (svgMax - svgGridBorder - (compWidth / 2))){
+              return svgMax + 95 - svgGridBorder - compWidth;
+            }
+            return x - (compWidth / 2) + 95;
+          })
+          .attr("y", () => { 
+            if(y < (svgGridBorder + (compHeight / 2))){
+              return svgGridBorder - 5 + (compHeight / 2);
+            } else if(y > (svgMax - svgGridBorder - (compHeight / 2))){
+              return svgMax - 5 - svgGridBorder - (compHeight / 2);
+            }
+            return y - 5;
+          });
+
+        if(this.fdCompToDrop.getInput() > 0) {
+          g.append("circle")
+            .attr("id", "input-" + newId)
+            .attr("r", 10)
+            .attr("fill", "white")
+            .attr("cx", () => {
+              if(x < (svgGridBorder + (compWidth / 2))){
+                return svgGridBorder;
+              } else if(x > (svgMax - svgGridBorder - (compWidth / 2))){
+                return svgMax - svgGridBorder - compWidth;
+              }
+              return x - (compWidth / 2);
+            })
+            .attr("cy", () => { 
+              if(y < (svgGridBorder + (compHeight / 2))){
+                return svgGridBorder + (compHeight / 2);
+              } else if(y > (svgMax - svgGridBorder - (compHeight / 2))){
+                return svgMax - svgGridBorder - (compHeight / 2);
+              }
+              return y;
             });
 
-        dragHandler(d3.select("#conception-grid-svg").selectAll("circle"));
+          g.append("text")
+            .attr("id", "input-text-" + newId)
+            .attr("fill", "black")
+            .html(this.fdCompToDrop.getInput())
+            .attr("x", () => {
+              if(x < (svgGridBorder + (compWidth / 2))){
+                return svgGridBorder - 5;
+              } else if(x > (svgMax - svgGridBorder - (compWidth / 2))){
+                return svgMax - 5 - svgGridBorder - compWidth;
+              }
+              return x - 5 - (compWidth / 2);
+            })
+            .attr("y", () => { 
+              if(y < (svgGridBorder + (compHeight / 2))){
+                return svgGridBorder + 5 + (compHeight / 2);
+              } else if(y > (svgMax - svgGridBorder - (compHeight / 2))){
+                return svgMax + 5 - svgGridBorder - (compHeight / 2);
+              }
+              return y + 5;
+            });
+        }
+        if(this.fdCompToDrop.getOutput() > 0) {
+          g.append("circle")
+            .attr("id", "output-" + newId)
+            .attr("r", 10)
+            .attr("fill", "white")
+            .attr("cx", () => {
+              if(x < (svgGridBorder + (compWidth / 2))){
+                return svgGridBorder + compWidth;
+              } else if(x > (svgMax - svgGridBorder - (compWidth / 2))){
+                return svgMax - svgGridBorder;
+              }
+              return x + (compWidth / 2);
+            })
+            .attr("cy", () => { 
+              if(y < (svgGridBorder + (compHeight / 2))){
+                return svgGridBorder + (compHeight / 2);
+              } else if(y > (svgMax - svgGridBorder - (compHeight / 2))){
+                return svgMax - svgGridBorder - (compHeight / 2);
+              }
+              return y;
+            });
+
+          g.append("text")
+            .attr("id", "output-text-" + newId)
+            .attr("fill", "black")
+            .html(this.fdCompToDrop.getOutput())
+            .attr("x", () => {
+              if(x < (svgGridBorder + (compWidth / 2))){
+                return svgGridBorder - 5 + compWidth;
+              } else if(x > (svgMax - svgGridBorder - (compWidth / 2))){
+                return svgMax - 5 - svgGridBorder;
+              }
+              return x - 5 + (compWidth / 2);
+            })
+            .attr("y", () => { 
+              if(y < (svgGridBorder + (compHeight / 2))){
+                return svgGridBorder + 5 + (compHeight / 2);
+              } else if(y > (svgMax - svgGridBorder - (compHeight / 2))){
+                return svgMax + 5 - svgGridBorder - (compHeight / 2);
+              }
+              return y + 5;
+            });
+        }
+        
+        const dragHandler = d3.drag()
+            .on("drag", function () {
+                const theCompWidth = Number.parseInt(d3.select(this).attr("width"));
+                const theCompHeight = Number.parseInt(d3.select(this).attr("height"));
+                d3.select(this)
+                  .attr("x", () => {
+                    if(d3.event.x < (svgGridBorder + (theCompWidth / 2))){
+                      return 10;
+                    } else if(d3.event.x > (svgMax - svgGridBorder - (theCompWidth / 2))){
+                      return svgMax - svgGridBorder - theCompWidth;
+                    }
+                    return d3.event.x - (theCompWidth / 2);
+                  })
+                  .attr("y", () => { 
+                    if(d3.event.y < (svgGridBorder + (theCompHeight / 2))){
+                      return svgGridBorder;
+                    } else if(d3.event.y > (svgMax - svgGridBorder - (theCompHeight / 2))){
+                      return svgMax - svgGridBorder - theCompHeight;
+                    }
+                    return d3.event.y - (theCompHeight / 2);
+                  });
+                const theCompId = d3.select(this).attr("id").replace('rect-','');
+                d3.select('#title-text-'+ theCompId)
+                  .attr("x", () => {
+                    if(d3.event.x < (svgGridBorder + (theCompWidth / 2))){
+                      return svgGridBorder + 95;
+                    } else if(d3.event.x > (svgMax - svgGridBorder - (theCompWidth / 2))){
+                      return svgMax + 95 - svgGridBorder - theCompWidth;
+                    }
+                    return d3.event.x - (theCompWidth / 2) + 95;
+                  })
+                  .attr("y", () => { 
+                    if(d3.event.y < (svgGridBorder + (theCompHeight / 2))){
+                      return svgGridBorder - 5 + (theCompHeight / 2);
+                    } else if(d3.event.y > (svgMax - svgGridBorder - (theCompHeight / 2))){
+                      return svgMax - 5 - svgGridBorder - (theCompHeight / 2);
+                    }
+                    return d3.event.y - 5;
+                  });
+                d3.select('#input-'+ theCompId)
+                  .attr("cx", () => {
+                    if(d3.event.x < (svgGridBorder + (theCompWidth / 2))){
+                      return svgGridBorder;
+                    } else if(d3.event.x > (svgMax - svgGridBorder - (theCompWidth / 2))){
+                      return svgMax - svgGridBorder - theCompWidth;
+                    }
+                    return d3.event.x - (theCompWidth / 2);
+                  })
+                  .attr("cy", () => { 
+                    if(d3.event.y < (svgGridBorder + (theCompHeight / 2))){
+                      return svgGridBorder + (theCompHeight / 2);
+                    } else if(d3.event.y > (svgMax - svgGridBorder - (theCompHeight / 2))){
+                      return svgMax - svgGridBorder - (theCompHeight / 2);
+                    }
+                    return d3.event.y;
+                  });
+                d3.select('#output-'+ theCompId)
+                .attr("cx", () => {
+                  if(d3.event.x < (svgGridBorder + (theCompWidth / 2))){
+                    return svgGridBorder + theCompWidth;
+                  } else if(d3.event.x > (svgMax - svgGridBorder - (theCompWidth / 2))){
+                    return svgMax - svgGridBorder;
+                  }
+                  return d3.event.x + (theCompWidth / 2);
+                })
+                .attr("cy", () => { 
+                  if(d3.event.y < (svgGridBorder + (theCompHeight / 2))){
+                    return svgGridBorder + (theCompHeight / 2);
+                  } else if(d3.event.y > (svgMax - svgGridBorder - (theCompHeight / 2))){
+                    return svgMax - svgGridBorder - (theCompHeight / 2);
+                  }
+                  return d3.event.y;
+                });
+
+                d3.select('#input-text-'+ theCompId)
+                  .attr("x", () => {
+                    if(d3.event.x < (svgGridBorder + (theCompWidth / 2))){
+                      return svgGridBorder - 5;
+                    } else if(d3.event.x > (svgMax - svgGridBorder - (theCompWidth / 2))){
+                      return svgMax - 5 - svgGridBorder - theCompWidth;
+                    }
+                    return d3.event.x - 5 - (theCompWidth / 2);
+                  })
+                  .attr("y", () => { 
+                    if(d3.event.y < (svgGridBorder + (theCompHeight / 2))){
+                      return svgGridBorder + 5 + (theCompHeight / 2);
+                    } else if(d3.event.y > (svgMax - svgGridBorder - (theCompHeight / 2))){
+                      return svgMax + 5 - svgGridBorder - (theCompHeight / 2);
+                    }
+                    return d3.event.y + 5;
+                  });
+                d3.select('#output-text-'+ theCompId)
+                .attr("x", () => {
+                  if(d3.event.x < (svgGridBorder + (theCompWidth / 2))){
+                    return svgGridBorder - 5 + theCompWidth;
+                  } else if(d3.event.x > (svgMax - svgGridBorder - (theCompWidth / 2))){
+                    return svgMax - 5 - svgGridBorder;
+                  }
+                  return d3.event.x - 5 + (theCompWidth / 2);
+                })
+                .attr("y", () => { 
+                  if(d3.event.y < (svgGridBorder + (theCompHeight / 2))){
+                    return svgGridBorder + 5 + (theCompHeight / 2);
+                  } else if(d3.event.y > (svgMax - svgGridBorder - (theCompHeight / 2))){
+                    return svgMax + 5 - svgGridBorder - (theCompHeight / 2);
+                  }
+                  return d3.event.y + 5;
+                });
+            });
+
+        dragHandler(d3.select("#conception-grid-svg").selectAll("rect"));
         this.fdCompToDrop = undefined;
       }
     };
@@ -126,9 +350,9 @@ export default class ConceptionGrid extends Vue {
   .grid {
     background-image: url("../assets/conceptiongrid.png");
     background-repeat: repeat;
-    min-height: 1000px;
+    min-height: 5000px;
     height: 100%;
-    min-width: 1000px;
+    min-width: 5000px;
     width: 100%;
   }
 </style>
--- vuejs_project/src/views/Home.vue
+++ vuejs_project/src/views/Home.vue
@@ -23,11 +23,11 @@ export default {
   data() {
     return {
       compBrutList: [new FDComponent("id0","FakeType1","FakeComp1", '#FFD800','autor',true,true,'icon','1.0','readme',false,'{}'),
-                            new FDComponent("id1","FakeType2","FakeComp2", '#FF0000','autor',true,true,'icon','1.0','readme',false,'{}'),
-                            new FDComponent("id2","FakeType1","FakeComp3", '#FFD800','autor',true,true,'icon','1.0','readme',false,'{}'),
-                            new FDComponent("id3","FakeType2","FakeComp4", '#FF0000','autor',true,true,'icon','1.0','readme',false,'{}'),
-                            new FDComponent("id4","FakeType1","FakeComp5", '#FFD800','autor',true,true,'icon','1.0','readme',false,'{}'),
-                            new FDComponent("id5","","FakeComp6", '#0094FF','autor',true,true,'icon','1.0','readme',false,'{}')]
+                            new FDComponent("id1","FakeType2","FakeComp2", '#FF0000','autor',true,false,'icon','1.0','readme',false,'{}'),
+                            new FDComponent("id2","FakeType1","FakeComp3", '#FFD800','autor',2,2,'icon','1.0','readme',false,'{}'),
+                            new FDComponent("id3","FakeType2","FakeComp4", '#FF0000','autor',true,3,'icon','1.0','readme',false,'{}'),
+                            new FDComponent("id4","FakeType1","FComp5", '#FFD800','autor',2,true,'icon','1.0','readme',false,'{}'),
+                            new FDComponent("id5","","FakeCompWithLongName", '#0094FF','autor',false,2,'icon','1.0','readme',false,'{}')]
     }
   }
 }
